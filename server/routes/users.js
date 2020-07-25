fdc3afdff306888637fa24109081f1c8ee0615cb
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-25 17:05:05 -0500

    restarted charity page branch

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const db = require('../database');
 
 router.get('/', (req, res) => {
-  // make db queryies and res.json the data
+  // make db queries and res.json the data
   res.json({data:['dolphins', 'manatees', 'sea turles']})
 })
 
