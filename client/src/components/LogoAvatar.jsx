7edf3e504149ed4c0590e6f4df9b6b04f3f030d3
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-27 16:50:11 -0500

    added some functions to the logoavatar component

--- client/src/components/LogoAvatar.jsx
+++ client/src/components/LogoAvatar.jsx
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import styles from '../styles/Charity.css';
 
 const LogoAvatar = () => {
+    const source = '';
+    const [prview, setPreview] = useState(null, src);
+
+    const onClose = () => {
+
+        setPreviewt(null);
+    }
+    
+    const onCrop = () => {
+
+        setPreviewt(preview);
+    }
 
     return (
         
@@ -9,7 +21,7 @@ const LogoAvatar = () => {
             <div className={styles.logoAvatarWrapper}>
 
                 <div className={styles.imageWrapper}>
-                    
+                    <img>{preview}</img>
                 </div>
                 <div> 
                     <h2>Charity/User name</h2>
