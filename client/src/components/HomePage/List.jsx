44bc73a178840cb3a5d3339a8ffc0fe05ff9a322
Jonathan Zurita <62966232+JonathanZurita@users.noreply.github.com>
2020-07-27 17:44:02 -0500

    list component created with test data endpoints. working for four types of lists

--- client/src/components/HomePage/List.jsx
+++ client/src/components/HomePage/List.jsx
@@ -1,65 +1,55 @@
 import React from 'react';
-//if we are a charity or donor
-    //list of charities or list of donor
+
 const List = ({ itemsData, charity, pickup }) => {
-itemsData = [{date: 'test', name: 'test', category: 'test category'},{date: 'test', name: 'test', category: 'test category'}]
-var title= 'test';
+    //test info
+itemsData = [{date: 'test', name: 'table', category: 'test category', value: '$500'},{zip: '78666', value: '$600', date: 'test', name: 'chairs', category: 'test category'}];
+charity = false;
+pickup = false;
+var title = 'test';
 var sortOptions = [];
-var rowHeaders = [];
-    function listMaker() {
-        if(charity) {
-            if(pickup) {
-                title='Items for Pickup';
-                //and sort options for zip, date and name of item
-                sortOptions = [item.zip, item.date, item.name];
-                //assign the row data to show zip code
-                rowHeaders = [item.zip, item.name, item.date, item.value];
-            } else {
-                title='Items Donated'
-                //otherwise, sort by date, name, est value
-                sortOptions = [item.date, item.name, item.value];
-                //assign row data for same as sort above
-                rowHeaders = [item.date, item.name, item.value, null];
-            }
+    //setting sorting options and titles for each list
+    if(charity) {
+        if(pickup) {
+            title='Items for Pickup';
+            sortOptions = ['zipcode', 'date','name'];
         } else {
-            if(pickup) {
-                title='Items for Pickup'
-                //sort by charity name, date, and est value, name
-                sortOptions = [item.name, item.date, item.value, item.charity];
-                //assign the row data to show ...
-                rowHeaders = [item.name, item.charity, item.date, item.value];
-            } else {
-                title='Items Donated'
-                //sort by charity name, date, value, name, maybe category
-                sortOptions = [item.charity, item.date, item.value, item.name, item.category];
-                //assign the row data to show ....
-                rowHeaders = [item.date, item.name, item.charity, item.value];
-            }
+            title='Items Donated'
+            sortOptions = ['date', 'name', 'value'];
+        }
+        
+    } else {
+        if(pickup) {
+            title='Items for Pickup'
+            sortOptions = ['name', 'date', 'value', 'charity'];
+        } else {
+            title='Items Donated'
+            sortOptions = ['charity', 'date', 'value', 'name', 'category'];
         }
     }
+
     return (
         <div className='listWrap'>
             <div className='listHeaderWrap'>
                 <select>
-                    <option value='1'>1</option>
-                    <option value='2'>2</option>
-                    <option value='3'>3</option>
-                    <option value='4'>4</option>
-                    <option value='5'>5</option>
-                    <option value='6'>6</option>
+                {sortOptions.map((item, i) => 
+                    <option value={i}>{item}</option>
+                    )
+                }
                 </select>
-                <div className="listTitle">'test title'</div>
+                <div className="listTitle">{title}</div>
 
             </div>
                 <div className='listRowWrap'>
                     {itemsData.map((item, i) => 
                         <div key={i} className='listItemRow'>
-                            <span className='listItemDate'>{item.date}</span>
-                            <span className='listItemName'>{item.name}</span>
-                            <span className='listItemCategory'>{item.category}</span>
-                            <span className='listItemCharity'>{item.charity}</span>
+                            <span className='listItemDate'> {item.date || ''} </span>
+                            <span className='listItemName'> {item.name || ''} </span>
+                            <span className='listItemCategory'> {item.category || ''} </span>
+                            <span className='listItemCharity'> {item.charity || ''} </span>
+                            <span className='listItemCharity'> {item.value || ''} </span>
+                            <span className='listItemCategory'> {item.zip || ''}</span>
                         </div>
-                        )}
+                    )}
                 </div>
         </div>
     )
