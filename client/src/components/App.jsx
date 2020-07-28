86b8ec0ad13896c3a14f866ca8e293ac276cf2a2
Peyton Perret <kpeyton95@gmail.com>
2020-07-28 13:59:42 -0500

    revert changes to test functoinality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import '../styles/App.css';
 import LandingPage from './Landing/landingPage.jsx'
-=======
 // import Charity from './Charity.jsx';
-import styles from '../styles/App.css';
+// import styles from '../styles/App.css';
 import Login from './Landing/Login.jsx';
 
->>>>>>> modified login
 
 export default class App extends Component {
   constructor(props) {
@@ -25,14 +22,11 @@ export default class App extends Component {
         <div className={styles.charityMainAppContainer}>
           <Charity imageUploadHandler={this.imageUploadHandler}/>
         </div>
-<<<<<<< HEAD
         
         <h1>Welcome to Blue Ocean!</h1>
         <LandingPage />
-=======
         <h1>Welcome to Blue Ocean!</h1>
         <Login />
->>>>>>> modified login
       </>
     );
   }
