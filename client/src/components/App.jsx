682a31e50b20287707ba03540a95b6d73286061e
Anthony Casper <tonycasper21@gmail.com>
2020-07-28 11:14:04 -0500

    Fixes random rebase leftover file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Charity from './Charity.jsx';
 import styles from '../styles/App.css';
 
-=======
-import styles from '../styles/App.css';
->>>>>>> Implementing login fucnctionality, break to help
 
 export default class App extends Component {
   constructor(props) {
