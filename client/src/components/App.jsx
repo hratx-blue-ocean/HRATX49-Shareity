63dee10d82821f6df96ea740e0b63cf290fb1341
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-29 11:34:29 -0500

    fixed moment error

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-// import '../styles/App.css';
 import LandingPage from './Landing/landingPage.jsx'
 import Charity from '../components/Charity/Charity.jsx';
 import AddItem from './Modals/AddItem.jsx'
-import styles from '../styles/App.css';
-import Login from './Landing/Login.jsx';
-import Charity from './Charity.jsx';
-// import styles from '../styles/App.css';
 import styles from '../styles/App.css';
 import Login from './Landing/Login.jsx';
 import {
@@ -32,21 +27,20 @@ export default class App extends Component {
   render() {
     return (
       <>
-        
-      <Router>
-        <Switch>
-          <Route path="/Charity">
-        <div className={styles.charityMainAppContainer}>
-          <Charity />
-        </div>
-        </Route>
-        <Route path="/">
-        <LandingPage />
-        <AddItem />
-        <h1>Welcome to Blue Ocean!</h1>
-        <Login />
-        </Route>
-        </Switch>
+        <Router>
+          <Switch>
+            <Route path="/Charity">
+          <div className={styles.charityMainAppContainer}>
+            <Charity />
+          </div>
+          </Route>
+          <Route path="/">
+          <LandingPage />
+          <AddItem />
+          <h1>Welcome to Blue Ocean!</h1>
+          <Login />
+          </Route>
+          </Switch>
         </Router>
       </>
     );
