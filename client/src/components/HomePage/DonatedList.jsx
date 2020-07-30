a651e4e7225e4d032db4fef7414ff08afbe62d13
Denee Lopez <59215877+deneelopez@users.noreply.github.com>
2020-07-29 20:29:31 -0500

    Update DonatedList.jsx

--- client/src/components/HomePage/DonatedList.jsx
+++ client/src/components/HomePage/DonatedList.jsx
@@ -3,7 +3,7 @@ import styles from '../../styles/lists.css';
 import { fakeData } from './fakeData.jsx';
 var _ = require('lodash');
 
-const DonatedList = ({ charity, rawData }) => {
+const DonatedList = ({ charity, rawData, taxData }) => {
     rawData = rawData || fakeData;
     var data = [];
     var totalVal = 0;
