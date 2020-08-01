d5de724002b264d4b20be780d088658441d51032
Anthony Casper <tonycasper21@gmail.com>
2020-08-01 12:18:28 -0500

    Fixes a couple typos in the text

--- client/src/components/aboutUs.jsx
+++ client/src/components/aboutUs.jsx
@@ -17,7 +17,7 @@ const AboutUs = () =>{
         </Link>
       </div>
       <div className={styles.container}>
-        <h3>Mission Statment</h3>
+        <h3>Mission Statement</h3>
         <img className={styles.image} src="./badCo.jpg"/>
         <p>
           Here at BadCo, we are committed to serving our community. As such, Sharity was created to connect wonderful and caring individuals with charities around the world. Sharity simplifies the donation process for both parties. And in combination with our amazing user base, we plan on making the world a better place.
@@ -27,11 +27,11 @@ const AboutUs = () =>{
         <h3>Individual Donors</h3>
         <img className={styles.image} src="./happy.jpg"/>
         <p className={styles.marginTop}>
-          Our Donors are the heart of our community. And without them (and BadCo), Sharity could never succeed. Individual Donors can use sharity to list items they no longer need or have picked up with the intent to give to charity. Instead of having to do everything yourself, BadCo does the heavy lifiting by listing your items for a charity to claim and pickup directly from you! Sign up today to join BadCo on our mission to save the world.
+          Our Donors are the heart of our community. And without them (and BadCo), Sharity could never succeed. Individual Donors can use Sharity to list items they no longer need or have picked up with the intent to give to charity. Instead of having to do everything yourself, BadCo does the heavy lifting by listing your items for a charity to claim and pickup directly from you! Sign up today to join BadCo on our mission to save the world.
         </p>
       </div>
       <div className={styles.container}>
-        <h3>Charitiy Support</h3>
+        <h3>Charity Support</h3>
         <img className={styles.image} src="./charity.jpg"/>
         <p className={styles.marginTop}>
           BadCo seeks to support all charities around the world. By creating a Sharity account for your charity, you will be able to browse countless amazing items being donated by individuals in your area. When you find items your charity can benefit from, BadCo makes it easy to claim the item, communicate with the Donor, and make the pickup! Sign up today to join BadCo on our mission to save the world.
