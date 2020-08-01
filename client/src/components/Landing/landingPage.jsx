443e72e68fa64daa3a77a7ed79d8ae1355c8876b
Benjamin Polancich <polancichben@gmail.com>
2020-08-01 09:55:29 -0500

    Hot fix to login

--- client/src/components/Landing/landingPage.jsx
+++ client/src/components/Landing/landingPage.jsx
@@ -11,7 +11,7 @@ var _ = require('lodash');
 let currentUser;
 let logged;
 
-if (localStorage.user.length !== 0) {
+if (!localStorage.length === 0) {
   currentUser = JSON.parse(localStorage.user).type;
   logged = true;
 } else {
